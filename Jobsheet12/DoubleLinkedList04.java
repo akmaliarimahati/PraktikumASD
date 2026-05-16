a76767e0a9ae15fd58b44b321ad132aa29233642
akmaliarimahati <akmaliarimahati09@gmail.com>
2026-05-16 13:21:26 +0700

    percobaan 1

---
+++ Jobsheet12/DoubleLinkedList04.java
@@ -0,0 +1,81 @@
+package Jobsheet12;
+
+public class DoubleLinkedList04 {
+    Node04 head;
+    Node04 tail;
+
+    public DoubleLinkedList04() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa04 data) {
+        Node04 newNode04 = new Node04(data);
+
+        if (isEmpty()) {
+            head = tail = newNode04;
+        } else {
+            newNode04.next = head;
+            head.prev = newNode04;
+            head = newNode04;
+        }
+
+    }
+
+    public void addLast(Mahasiswa04 data) {
+        Node04 newNode04 = new Node04(data);
+
+        if (isEmpty()) {
+            head = tail = newNode04;
+        } else {
+            tail.next = newNode04;
+            newNode04.prev = tail;
+            tail = newNode04;
+        }
+
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa04 data) {
+        Node04 current = head;
+
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Data Dengan NIM " + keyNim + " Tidak Ditemukan");
+            return;
+        }
+
+        Node04 newNode04 = new Node04(data);
+
+        // jika current adalah tail, node baru ditmbahkan di akhir
+        if (current == tail) {
+            newNode04.prev = current;
+            current.next = newNode04;
+            tail = newNode04;
+        } else { // node barus disisipkan di tengah
+            newNode04.prev = current;
+            newNode04.next = current.next;
+            current.next.prev = newNode04;
+            current.next = newNode04;
+        }
+        System.out.println("Data Berhasil DIsisipkan Setelah NIM " + keyNim);
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Linked List Masih Kosong");
+            return;
+        }
+
+        Node04 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;        }
+    }
+}
---
+++ Jobsheet12/DoubleLinkedListMain04.java
@@ -0,0 +1,82 @@
+package Jobsheet12;
+
+import java.util.Scanner;
+
+public class DoubleLinkedListMain04 {
+    // metthod untuk menambahkan data mahasiswa
+    public static Mahasiswa04 inputMahasiswa(Scanner scan) {
+        System.out.print("Masukkan NIM   : ");
+        String nim = scan.nextLine();
+        System.out.print("Masukkan Nama  : ");
+        String nama = scan.nextLine();
+        System.out.print("Masukkan Kelas : ");
+        String kelas = scan.nextLine();
+        System.out.print("Masukkan IPK   : ");
+        double ipk = scan.nextDouble();
+        scan.nextLine();
+
+        // mengmbalikan nilai mahasiswa 04
+        return new Mahasiswa04(nim, nama, kelas, ipk);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DoubleLinkedList04 list = new DoubleLinkedList04();
+        int pilihan;
+
+        do {
+            System.out.println("\n======== MENU DOUBLE LINKED LIST ==========");
+            System.out.println("1. Tambah Data Di Awal");
+            System.out.println("2. Tambah Data Di Akhir");
+            System.out.println("3. Sisipkan Data Di Tengah (Setelah NIM)");
+            System.out.println("4. Hapus Data Di Awal");
+            System.out.println("5. Hapus Data Di Akhir");
+            System.out.println("6. Tampilkan Data");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih Menu: ");
+            pilihan = scan.nextInt();
+            scan.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    Mahasiswa04 mhsAwal = inputMahasiswa(scan);
+                    list.addFirst(mhsAwal);
+                    break;
+
+                case 2:
+                    Mahasiswa04 mhsAkhir = inputMahasiswa(scan);
+                    list.addLast(mhsAkhir);
+                    break;
+
+                case 3:
+                    System.out.print("Masukkkan NIM Yang Di Cari: ");
+                    String keyNim = scan.nextLine();
+
+                    System.out.println("Masukkan Data Baru: ");
+                    Mahasiswa04 dataBaru = inputMahasiswa(scan);
+                    list.insertAfter(keyNim, dataBaru);
+                    break;
+
+                // case 4:
+                // list.removeFirst();
+                // break;
+
+                // case 5:
+                // list.removeLast();
+                // break;
+
+                case 6:
+                    list.print();
+                    break;
+
+                case 0:
+                    System.out.println("Program Selesai.");
+                    break;
+
+                default:
+                    System.out.println("Menu Tidak Valid");
+            }
+        } while (pilihan != 0);
+        scan.close();
+    }
+}
